88d86a68eec84bc6ce879018defc11533bb8c714
Minuke <nakamadeath@gmail.com>
2026-08-16 17:25:13 +0200

    add dashboard and habits pages with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./features/dashboard/pages/dashboard-page/dashboard-page').then((c) => c.DashboardPage),
+  },
+  {
+    path: 'habits',
+    loadComponent: () => import('./features/habits/pages/habits-page/habits-page').then((c) => c.HabitsPage),
+  },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard' },
+];
---
+++ src/app/features/habits/pages/habits-page/habits-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habits-page',
+  imports: [],
+  templateUrl: './habits-page.html',
+  styleUrl: './habits-page.scss',
+})
+export class HabitsPage {}
