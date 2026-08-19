f3d5063aa97a4338e4f63c2b9e8df7b7ec48b5cb
Minuke <nakamadeath@gmail.com>
2026-08-19 23:38:13 +0200

    add routing for login and register pages; implement route protection for dashboard and habits

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,27 @@
 import { Routes } from '@angular/router';
+import { authGuard } from '@core/guards/auth-guard';
+
 
 export const routes: Routes = [
   {
+    path: 'login',
+    loadComponent: () => import('./features/auth/pages/login-page/login-page').then((c) => c.LoginPage),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/auth/pages/register-page/register-page').then((c) => c.RegisterPage),
+  },
+  {
     path: 'dashboard',
-    loadComponent: () => import('./features/dashboard/pages/dashboard-page/dashboard-page').then((c) => c.DashboardPage),
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/dashboard/pages/dashboard-page/dashboard-page').then((m) => m.DashboardPage),
   },
   {
     path: 'habits',
-    loadComponent: () => import('./features/habits/pages/habits-page/habits-page').then((c) => c.HabitsPage),
+    canActivate: [authGuard],
+    loadComponent: () => import('./features/habits/pages/habits-page/habits-page').then((m) => m.HabitsPage),
   },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '**', redirectTo: 'dashboard' },
