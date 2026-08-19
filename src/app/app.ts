3189af1a6371c05d5257bdb0a59ebddf9808e97f
Minuke <nakamadeath@gmail.com>
2026-08-19 23:43:17 +0200

    refactor navigation and authentication handling; update app component and styles for user display and logout functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AuthStore } from '@core/services/auth-store';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
+  protected readonly authStore = inject(AuthStore);
+  private readonly router = inject(Router);
 
+  protected onLogout(): void {
+    this.authStore.logout();
+    this.router.navigateByUrl('/login');
+  }
 }
